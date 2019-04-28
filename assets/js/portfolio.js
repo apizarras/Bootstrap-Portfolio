889667db0cf003504126d71971fe4b003368540a
apizarras <angelalee.p@gmail.com>
2019-04-27 19:24:52 -0500

    add project cards on portfolio page

---
+++ assets/js/portfolio.js
@@ -0,0 +1,11 @@
+// $('.grid').masonry({
+//     // options
+//     itemSelector: '.grid-item',
+//     columnWidth: 200
+//   });
+
+$('.grid').masonry({
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true
+});
